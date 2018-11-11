02fe4703cceee02a9e878f4c931bc87ced847162
Tim <InspektorTimchik@gmail.com>
2018-11-11 17:09:49 +0200

    20181111_17_09

---
+++ dgr_20181111.py
@@ -0,0 +1,142 @@
+# coding: utf-8
+str1 = "Hello"
+str2 = "there"
+bob = str1 + str2
+bob
+apple = input("Enter:")
+x=apple -10
+x
+fruit = banana
+fruit = "banana"
+letter = fruit[1]
+letter
+x=3
+w=fruit[x-1]
+w
+zot ="abc"
+print(zot[5])
+print(len(fruit))
+index =0
+while index < len(fruit):
+    letter = fruit[index]
+    print(index,letter)
+    index = index + 1
+    
+for letter in fruit:
+    print(letter)
+    
+index=0
+while index < len(fruit):
+    letter = fruit[index]
+    print(letter)
+    index=index+1
+    
+word ="banana"
+count=0
+for letter in word:
+    if letter =="a":
+        count=count+1
+print(count)
+s="Monty Python"
+print(s[0:4])
+print(s[6])
+print(s[5])
+print(s[6])
+print(s[6:7])
+print(s[6:11])
+print(s[6:12])
+print(s[:2])
+print(s[8:])
+print(s[:])
+a ="Hello"
+b=a+"there"
+b
+c= a+""+"There"
+c
+c= a+" "+"There"
+c
+"n" in fruit
+"m" in fruit
+"nan" in fruit
+if "a" in fruit:
+    print("Found it!")
+if word == "banana:"
+if word == "banana":
+    print('all right, bananaas.')
+if word<'banana':
+    print('Your word,'+word+',comes before banana.')
+elif word > 'banana':
+    print('Your word,'+word+',comes after banana.')
+    else:
+if word<'banana':
+    print('Your word,'+word+',comes before banana.')
+elif word > 'banana':
+    print('Your word,'+word+',comes after banana.')
+else:
+    print('All right, bananas.')
+    
+word = ban
+word = 'ban'
+if word<'banana':
+    print('Your word,'+word+',comes before banana.')
+elif word > 'banana':
+    print('Your word,'+word+',comes after banana.')
+else:
+    print('All right, bananas.')
+    
+greet = 'Hello Bob'
+zap = greet.lower()
+zap
+greet
+print('Hi There'.lower())
+print('Hi There'.lower(1))
+stuff ='Hello world'
+dir(stuff)
+str.replace(old, new[, count])
+fruit
+pos = fruit.find('na')
+pos
+aa = fruit.find('z')
+aa
+nnn = greet.upper()
+nnn
+www = greet.lower()
+www
+nstr =greet.replace('o','X')
+nstr
+nstr =greet.replace('Bob','Jane')
+nstr
+greet = ' Hello Bob '
+greet.lstrip()
+greet
+greet.rstrip()
+greet.strip()
+line ='Please have a nice day'
+line.startswith('Please')
+line.startswith('p')
+data = ' From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16:2008'
+atpos = data.find('@')
+apos
+atpos
+sppos =data.find('',atpos)
+sppos
+host =data[atpos+1 : sppos]
+host
+get_ipython().magic(u'history > dgr_20181111_16_44.py')
+get_ipython().magic(u'cat dgr_20181111_16_44.py')
+get_ipython().magic(u'cat dgr_20181111_16_44.py ./')
+get_ipython().magic(u'history > dgr_20181111_16_44.py ./')
+get_ipython().magic(u'ls -l')
+dgr_20181111_16_44.py
+get_ipython().magic(u'save ')
+get_ipython().magic(u'history > ./dgr_20181111.py')
+get_ipython().magic(u'ls -l')
+get_ipython().magic(u'history > ./ dgr_20181111.py')
+get_ipython().magic(u'ls -l')
+get_ipython().magic(u'history > dgr_20181111.py')
+get_ipython().magic(u'ls -l')
+get_ipython().magic(u'history > dgr_20181111.txt')
+get_ipython().magic(u'ls -l')
+help
+help()
+get_ipython().magic(u'save current_session ~0/')
